5a0607b7108145c77c0e88d301ef7b57279987b6
Michael Dzjaparidze <michael.dzjaparidze@gmail.com>
2020-01-23 17:41:17 +0100

    Create mongodb database and user on initial mount

---
+++ init-mongo.sh
@@ -0,0 +1,5 @@
+mongo -- "$MONGO_INITDB_DATABASE" <<EOF
+    db.getSiblingDB('admin').auth('$MONGO_INITDB_ROOT_USERNAME', '$MONGO_INITDB_ROOT_PASSWORD');
+
+    db.createUser({user: '$MONGO_INITDB_USERNAME', pwd: '$MONGO_INITDB_PASSWORD', roles: ["readWrite"]});
+EOF
